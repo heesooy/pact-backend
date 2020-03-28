f652b3940f4f80957055f4f53d0e995e1ddf6e5b
Heesoo Mac <heesoo4199@gmail.com>
2020-03-28 18:45:52 -0500

    Add neo4j and implement friends API endpoints

--- db/User.js
+++ db/User.js
@@ -1,4 +1,5 @@
 const connectToDatabase = require('../db');
+const neo4j = require('../neo4j')
 const uuid = require('uuid');
 
 module.exports.create = async (user) => {
@@ -6,6 +7,20 @@ module.exports.create = async (user) => {
   user.user_id = uuid();
   await client.query('INSERT INTO User SET ?', user);
   client.quit();
+
+  const driver = await neo4j.connectDriver();
+  const session = driver.session();
+
+  try {
+    await session.run('CREATE (n:User { user_id: $user_id })', { user_id: user.user_id });
+  } catch (error) {
+    console.error("Neo4j: " + error);
+    // TODO: undo MySQL transaction and proper error handling in invoker (i.e. when this returns null)
+    // return null;
+  } finally {
+    await session.close();
+    await driver.close();
+  }
 
   console.log(user);
   return user;
@@ -21,3 +36,149 @@ module.exports.findByEmail = async (email) => {
   console.log(user[0]);
   return user[0];
 };
+
+module.exports.getFriends = async (id) => {
+  const driver = await neo4j.connectDriver();
+  const session = driver.session();
+
+  try {
+    const result = await session.run(
+      'MATCH (a:User { user_id: $user_id })-[:FRIENDS]-(b:User) RETURN b.user_id',
+      { user_id: id }
+    );
+    return result.records.map(x => x._fields[0]);;
+  } catch (error) {
+    console.error("Neo4j: " + error);
+    return null;
+  } finally {
+    await session.close();
+    await driver.close();
+  }
+}
+
+module.exports.areFriends = async (id1, id2) => {
+  const driver = await neo4j.connectDriver();
+  const session = driver.session();
+
+  try {
+    const result = await session.run(
+      'MATCH (a:User)-[:FRIENDS]-(b:User) \
+      WHERE a.user_id = $id1 AND b.user_id = $id2 \
+      RETURN b.user_id',
+      { id1: id1, id2: id2 }
+    );
+    return result.records.length == 1;
+  } catch (error) {
+    console.error("Neo4j: " + error);
+    return null;
+  } finally {
+    await session.close();
+    await driver.close();
+  }
+}
+
+module.exports.addFriend = async (id1, id2) => {
+  const driver = await neo4j.connectDriver();
+  const session = driver.session();
+
+  try {
+    await session.run(
+      'MATCH (a:User), (b:User) \
+      WHERE a.user_id = $id1 AND b.user_id = $id2 \
+      CREATE (a)-[r:FRIENDS]->(b)',
+      { id1: id1, id2: id2 }
+    );
+    return true;
+  } catch (error) {
+    console.error("Neo4j: " + error);
+    return null;
+  } finally {
+    await session.close();
+    await driver.close();
+  }
+}
+
+module.exports.getRequests = async (id) => {
+  const driver = await neo4j.connectDriver();
+  const session = driver.session();
+
+  try {
+    const result = await session.run(
+      'MATCH (a:User { user_id: $user_id })<-[:REQUESTED]-(b:User) \
+      RETURN b.user_id',
+      { user_id: id }
+    );
+    return result.records.map(x => x._fields[0]);
+  } catch (error) {
+    console.error("Neo4j: " + error);
+    return null;
+  } finally {
+    await session.close();
+    await driver.close();
+  }
+}
+
+module.exports.hasRequested = async (id1, id2) => {
+  const driver = await neo4j.connectDriver();
+  const session = driver.session();
+
+  try {
+    const result = await session.run(
+      'MATCH (a:User)-[:REQUESTED]-(b:User) \
+      WHERE a.user_id = $id1 AND b.user_id = $id2 \
+      RETURN b.user_id',
+      { id1: id1, id2: id2 }
+    );
+    return result.records.length == 1;
+  } catch (error) {
+    console.error("Neo4j: " + error);
+    return null;
+  } finally {
+    await session.close();
+    await driver.close();
+  }
+}
+
+// Create request from id1 to id2
+module.exports.createRequest = async (id1, id2) => {
+  const driver = await neo4j.connectDriver();
+  const session = driver.session();
+
+  try {
+    await session.run(
+      'MATCH (a:User), (b:User) \
+      WHERE a.user_id = $id1 AND b.user_id = $id2 \
+      CREATE (a)-[r:REQUESTED]->(b)',
+      { id1: id1, id2: id2 }
+    );
+    return true;
+  } catch (error) {
+    console.error("Neo4j: " + error);
+    return null;
+  } finally {
+    await session.close();
+    await driver.close();
+  }
+}
+
+// Delete request BETWEEN (directionless) id1 and i2
+module.exports.deleteRequest = async (id1, id2) => {
+  const driver = await neo4j.connectDriver();
+  const session = driver.session();
+
+  try {
+    await session.run(
+      'MATCH (a:User)-[r:REQUESTED]-(b:User) \
+      WHERE a.user_id = $id1 AND b.user_id = $id2 \
+      DELETE r',
+      { id1: id1, id2: id2 }
+    );
+    return true;
+  } catch (error) {
+    console.error("Neo4j: " + error);
+    return null;
+  } finally {
+    await session.close();
+    await driver.close();
+  }
+}
---
+++ friends/FriendsHandler.js
@@ -0,0 +1,243 @@
+const User = require('../db/User');
+const jwt = require('jsonwebtoken');
+
+/**
+ * Functions
+ */
+
+/*
+ * GET /friends/
+ */
+module.exports.getFriends = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const token = event.headers.Authorization;
+  const decoded = decodeJwt(token);
+  if (!decoded) // token empty or invalid
+    return { 
+      statusCode: 403,
+      body: JSON.stringify({ message: 'Forbidden: missing or invalid JWT.'  })
+    };
+
+  return await getFriends(decoded.id)
+    .then(resp => ({
+      statusCode: 200,
+      body: JSON.stringify(resp)
+    }))
+    .catch(err => ({
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({ stack: err.stack, message: err.message })
+    }));
+};
+
+/*
+ * GET /friends/request/
+ */
+module.exports.getRequests = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const token = event.headers.Authorization;
+  const decoded = decodeJwt(token);
+  if (!decoded) // token empty or invalid
+    return { 
+      statusCode: 403,
+      body: JSON.stringify({ message: 'Forbidden: missing or invalid JWT.'  })
+    };
+  
+  return await getRequests(decoded.id)
+    .then(resp => ({
+      statusCode: 200,
+      body: JSON.stringify(resp)
+    }))
+    .catch(err => ({
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({ stack: err.stack, message: err.message })
+    }));
+};
+
+/*
+ * POST /friends/request/send/
+ */
+module.exports.sendRequest = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const token = event.headers.Authorization;
+  const decoded = decodeJwt(token);
+  if (!decoded) // token empty or invalid
+    return { 
+      statusCode: 403,
+      body: JSON.stringify({ message: 'Forbidden: missing or invalid JWT.'  })
+    };
+
+  return await sendRequest(decoded.id, JSON.parse(event.body))
+    .then(resp => ({
+      statusCode: 200,
+      body: JSON.stringify(resp)
+    }))
+    .catch(err => ({
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({ stack: err.stack, message: err.message })
+    }));
+};
+
+/*
+ * POST /friends/request/accept/
+ */
+module.exports.acceptRequest = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const token = event.headers.Authorization;
+  const decoded = decodeJwt(token);
+  if (!decoded) // token empty or invalid
+    return { 
+      statusCode: 403,
+      body: JSON.stringify({ message: 'Forbidden: missing or invalid JWT.'  })
+    };
+
+  return await acceptRequest(decoded.id, JSON.parse(event.body))
+    .then(resp => ({
+      statusCode: 200,
+      body: JSON.stringify(resp)
+    }))
+    .catch(err => ({
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({ stack: err.stack, message: err.message })
+    }));
+};
+
+/*
+ * POST /friends/request/decline/
+ */
+module.exports.declineRequest = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const token = event.headers.Authorization;
+  const decoded = decodeJwt(token);
+  if (!decoded) // token empty or invalid
+  return { 
+    statusCode: 403,
+    body: JSON.stringify({ message: 'Forbidden: missing or invalid JWT.'  })
+  };
+
+  return await declineRequest(decoded.id, JSON.parse(event.body))
+    .then(resp => ({
+      statusCode: 200,
+      body: JSON.stringify(resp)
+    }))
+    .catch(err => ({
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({ stack: err.stack, message: err.message })
+    }));
+};
+
+/**
+ * Helpers
+ */
+
+function decodeJwt(token) {
+  console.log("decodeJwt");
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded;
+  } catch(err) {
+    console.error("decodeJwt error " + err);
+    return null;
+  }
+}
+
+function getFriends(id) {
+  console.log("getFriends");
+
+  return User.getFriends(id)
+    .then(friends =>
+      !friends
+        ? Promise.reject(new Error('Error fetching friends.'))
+        : ({ friends: friends })
+    );
+}
+
+function getRequests(id) {
+  console.log("getRequest");
+  
+  return User.getRequests(id)
+    .then(requests => 
+      !requests
+        ? Promise.reject(new Error('Error fetching friend requests.'))
+        : ({ requests: requests })
+    );
+}
+
+function sendRequest(id, eventBody) {
+  console.log("sendRequest");
+
+  return User.areFriends(id, eventBody.user_id) // check if somehow already friends
+    .then(friended => {
+      if (friended == null)
+        return Promise.reject(new Error('Error checking if already friends.'));
+      if (friended == true)
+        return Promise.reject(new Error('Users are already friends.'));
+    }).then(() => 
+      User.createRequest(id, eventBody.user_id) // create 'REQUESTED' relationship
+    ).then(success => 
+      !success
+        ? Promise.reject(new Error('Error fetching friend requests.'))
+        : eventBody
+    );
+}
+
+function acceptRequest(id, eventBody) {
+  console.log("acceptRequest");
+
+  return User.hasRequested(id, eventBody.user_id) // check if request even exists
+    .then(requested => {
+      if (requested == null)
+        return Promise.reject(new Error('Error checking if friend request exists.'));
+      if (requested == false)
+        return Promise.reject(new Error('User has not received friend request.'));
+    }).then(() => 
+      User.areFriends(id, eventBody.user_id) // check if somehow already friends
+    ).then(friended => {
+      if (friended == null)
+        return Promise.reject(new Error('Error checking if already friends.'));
+      if (friended == true)
+        return Promise.reject(new Error('Users are already friends.'));
+    }).then(() =>
+      User.deleteRequest(id, eventBody.user_id) // delete 'REQUESTED' relationship
+    ).then(success => 
+      !success
+        ? Promise.reject(new Error('Error deleting friend request.'))
+        : eventBody
+    ).then(() => 
+      User.addFriend(id, eventBody.user_id) // replace with 'FRIENDS' relationship
+    ).then(success =>
+      !success
+        ? Promise.reject(new Error('Error adding friend.'))
+        : eventBody
+    );
+}
+
+function declineRequest(id, eventBody) {
+  console.log("declineRequest");
+
+  return User.hasRequested(id, eventBody.user_id) // check if request even exists
+    .then(requested => {
+      if (requested == null)
+        return Promise.reject(new Error('Error checking if friend request exists.'));
+      if (requested == false)
+        return Promise.reject(new Error('User has not received friend request.'));
+    }).then(() => 
+      User.areFriends(id, eventBody.user_id) // check if somehow already friends
+    ).then(friended => {
+      if (friended == null)
+        return Promise.reject(new Error('Error checking if already friends.'));
+      if (friended == true)
+        return Promise.reject(new Error('Users are already friends.'));
+    }).then(() => 
+      User.deleteRequest(id, eventBody.user_id) // delete 'REQUESTED' relationship
+    ).then(success => 
+      !success
+        ? Promise.reject(new Error('Error deleting friend request.'))
+        : eventBody
+    );
+}
